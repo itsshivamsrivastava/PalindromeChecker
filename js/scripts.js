2ce3331b12465bf1337b2aba919a7271676dc1f8
Shivam Srivastava <73056786+itsshivamsrivastava@users.noreply.github.com>
2022-09-16 02:40:44 +0530

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+/* 
+    Project Name: Palindrome Checker 
+    Author: Shivam Srivastava
+    Technology Used: HTML, CSS, JavaScript
+*/
+const input = document.querySelector(".inputs input");
+const button = document.querySelector(".inputs button");
+const clearBtn = document.querySelector(".inputs clearBtn");
+result = document.querySelector(".info-txt");
+
+let filterInput;
+result.style.display = "block";
+
+checkPalindrome = () => {
+    let inputVal = input.value;
+    let reverseInput = inputVal.split('').reverse().join('');
+    if (inputVal === reverseInput) {
+        result.style.display = "block";
+        document.querySelector(".info-txt").innerHTML = "Yes, it is a Palindrome";
+        console.log("Yes, it is a Palindrome");
+    } else {
+        result.style.display = "block";
+        document.querySelector(".info-txt").innerHTML = "No, it is a Palindrome";
+        console.log("No, it is not a Palindrome");
+    }
+}
+button.addEventListener("click", checkPalindrome);
+
+function clearInput() {
+    input.value = "";
+    result.style.display = "none"
+}
+clearBtn.addEventListener("click", clearInput);
